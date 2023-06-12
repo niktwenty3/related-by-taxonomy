54edb4ddbb7e4779bb645d00b854f6c44174ff7f
Nik <nigniatovic@haproxy.com>
2023-06-12 07:36:23 +0200

    Added some variables to keep some information on score details

--- src/Relbytaxonomy.php
+++ src/Relbytaxonomy.php
@@ -21,6 +21,7 @@ class Relbytaxonomy extends \Statamic\Tags\Tags
 
         // Split the taxonomies parameter into an array
         $taxonomies = explode('|', $this->params->get('taxonomies'));
+
         
         // Split modifier scores parameter into an array
         $modifiers =  explode('|', $this->params->get('modifiers'));
@@ -34,7 +35,7 @@ class Relbytaxonomy extends \Statamic\Tags\Tags
         // Array to store related entries
         $relatedEntries = [];
 
-
+        $found_in_taxonomies = [];
         // Loop through all entries
         foreach ($allEntries as $otherEntry) {
 
@@ -44,32 +45,48 @@ class Relbytaxonomy extends \Statamic\Tags\Tags
             // Initialize score and index
             $score = 0;
             $index = 0;
+            $commonTags = [];
+            $found_in_taxonomies = [];
 
             // Loop through each taxonomy
             foreach ($taxonomies as $taxonomy) {
+                \Log::info($taxonomy);
                 
                 // if both entries have the taxonomy
                 if ($entry->get($taxonomy) && $otherEntry->get($taxonomy)) {
+                    
+
+                        
+                        // Get the shared items between the two entries
+                        $sharedItems = array_intersect($entry->get($taxonomy), $otherEntry->get($taxonomy));
+
+                        // Merge the shared items into the common tags array
+                        $commonTags = array_merge($commonTags, $sharedItems);
+                        array_push($found_in_taxonomies, $taxonomy);
+
+                        
+                        // Increase the score by the number of shared items multiplied by the modifier
+                        $score += count($sharedItems)*(float)$modifiers[$index];
                 
-                    // Get the shared items
-                    $sharedItems = array_intersect($entry->get($taxonomy), $otherEntry->get($taxonomy));
-                
-                    // Add the score to the total score multiplied by the modifier for the taxonomy
-                    $score += count($sharedItems)*(float)$modifiers[$index];
+                    
                 }
 
                 // Increment the index
                 $index++;
             }
 
+            
             // [TODO] Add other scoring rules here...
-
+            
             // If the score is greater than 0, add the entry to the related entries array
             if ($score > 0) {
+                
                 $relatedEntries[] = [
                     'entry' => $otherEntry,
                     'score' => $score, // Add the score in order to sort by score
                     'date' => $otherEntry->date()->timestamp, // Add the timestamp of the entry's date in order to sort by date
+                    'common_tags' => implode(", ", $commonTags),
+                    'found_in_taxonomies' => implode(", ", $found_in_taxonomies),
                 ];
             }
         }
@@ -82,18 +99,23 @@ class Relbytaxonomy extends \Statamic\Tags\Tags
         // Limit the number of related entries
         $relatedEntries = array_slice($relatedEntries, 0, $entriesLimit);
 
-        // Sort the limited related entries by date in descending order
-        usort($relatedEntries, function ($a, $b) {
-            return $b['date'] <=> $a['date'];
-        });
+        // // Sort the limited related entries by date in descending order
+        // usort($relatedEntries, function ($a, $b) {
+        //     return $b['date'] <=> $a['date'];
+        // });
         
+        $result = array_map(function ($item) {
+            return $item['entry']->toAugmentedArray();
+        }, $relatedEntries);
+
+        for ($i = 0; $i < count($result); $i++) {
+            $result[$i]['score'] = $relatedEntries[$i]['score'];
+            $result[$i]['common_tags'] = $relatedEntries[$i]['common_tags'];
+            $result[$i]['found_in_taxonomies'] = $relatedEntries[$i]['found_in_taxonomies'];
+        }
+
         return [
-            'related_entries' => array_map(function ($item) {
-                return $item['entry']->toAugmentedArray();
-            }, $relatedEntries),
-            'scores' =>  array_map(function ($item) {
-                return $item['score'];
-            }, $relatedEntries)
+            'related_entries' => $result
         ];
     }
 }
