f7f8733f926e686126b71a9e37fb8b79cb023343
Nik <nigniatovic@haproxy.com>
2023-06-12 09:03:20 +0200

    Added title matching and cleaned up a bit

--- src/Relbytaxonomy.php
+++ src/Relbytaxonomy.php
@@ -4,6 +4,7 @@ namespace Niktwenty3\RelatedByTaxonomy;
 
 use Statamic\Tags\Tags;
 use Statamic\Facades\Entry;
+use Yooper\StopWords;
 
 class Relbytaxonomy extends \Statamic\Tags\Tags
 {
@@ -35,7 +36,6 @@ class Relbytaxonomy extends \Statamic\Tags\Tags
         // Array to store related entries
         $relatedEntries = [];
 
-        $found_in_taxonomies = [];
         // Loop through all entries
         foreach ($allEntries as $otherEntry) {
 
@@ -45,41 +45,56 @@ class Relbytaxonomy extends \Statamic\Tags\Tags
             // Initialize score and index
             $score = 0;
             $index = 0;
+            // Array to store common tags for each entry
             $commonTags = [];
+            // Array to store taxonomies that had a match
             $found_in_taxonomies = [];
+
+            // Tokenize the titles into arrays
+            $entryTitleTokens = array_unique(explode(' ', strtolower($entry->get('title'))));
+            $otherEntryTitleTokens = array_unique(explode(' ', strtolower($otherEntry->get('title'))));
+
+            // Remove stop words from the title tokens
+            $stopWords = new Yooper\StopWords();
+            $stopWordsList = $stopWords->getStopWords('english');
+            $entryTitleTokens = array_diff($entryTitleTokens, $stopWordsList);
+            $otherEntryTitleTokens = array_diff($otherEntryTitleTokens, $stopWordsList);
+
+            // Count the total number of unique words in the entry's title
+            $totalWordsInEntryTitle = count($entryTitleTokens);
+
+            // Determine how many of those words were found in the other entry's title
+            $commonWordsCount = count(array_intersect($entryTitleTokens, $otherEntryTitleTokens));
+
+            // Compute the percentage of words found in the other entry's title
+            $wordMatchPercentage = ($commonWordsCount / $totalWordsInEntryTitle) * 100;
 
             // Loop through each taxonomy
             foreach ($taxonomies as $taxonomy) {
-                \Log::info($taxonomy);
-                
+
                 // if both entries have the taxonomy
                 if ($entry->get($taxonomy) && $otherEntry->get($taxonomy)) {
                     
-
-                        
                         // Get the shared items between the two entries
                         $sharedItems = array_intersect($entry->get($taxonomy), $otherEntry->get($taxonomy));
 
                         // Merge the shared items into the common tags array
                         $commonTags = array_merge($commonTags, $sharedItems);
                         array_push($found_in_taxonomies, $taxonomy);
-
                         
                         // Increase the score by the number of shared items multiplied by the modifier
                         $score += count($sharedItems)*(float)$modifiers[$index];
-                
                     
                 }
 
                 // Increment the index
                 $index++;
             }
-
             
             // [TODO] Add other scoring rules here...
             
             // If the score is greater than 0, add the entry to the related entries array
-            if ($score > 0) {
+            if ($score > 0 || $wordMatchPercentage > 0) {
                 
                 $relatedEntries[] = [
                     'entry' => $otherEntry,
@@ -87,6 +102,7 @@ class Relbytaxonomy extends \Statamic\Tags\Tags
                     'date' => $otherEntry->date()->timestamp, // Add the timestamp of the entry's date in order to sort by date
                     'common_tags' => implode(", ", $commonTags),
                     'found_in_taxonomies' => implode(", ", $found_in_taxonomies),
+                    'word_match_percentage' => $wordMatchPercentage,
                 ];
             }
         }
@@ -99,19 +115,22 @@ class Relbytaxonomy extends \Statamic\Tags\Tags
         // Limit the number of related entries
         $relatedEntries = array_slice($relatedEntries, 0, $entriesLimit);
 
-        // // Sort the limited related entries by date in descending order
-        // usort($relatedEntries, function ($a, $b) {
-        //     return $b['date'] <=> $a['date'];
-        // });
+        // Sort the limited related entries by date in descending order
+        usort($relatedEntries, function ($a, $b) {
+            return $b['word_match_percentage'] <=> $a['word_match_percentage'];
+        });
         
         $result = array_map(function ($item) {
             return $item['entry']->toAugmentedArray();
         }, $relatedEntries);
 
+        // Add the score, common tags, and found in taxonomies to the result
+        // Doing it this way because if I try to map weird things happen
         for ($i = 0; $i < count($result); $i++) {
             $result[$i]['score'] = $relatedEntries[$i]['score'];
             $result[$i]['common_tags'] = $relatedEntries[$i]['common_tags'];
             $result[$i]['found_in_taxonomies'] = $relatedEntries[$i]['found_in_taxonomies'];
+            $result[$i]['word_match_percentage'] = $relatedEntries[$i]['word_match_percentage'];
         }
 
         return [
